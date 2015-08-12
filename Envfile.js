0bcc3716fc28c25eb70009a49c6a7a3c58a1e5b4
evio <shenyunjie@51zhangdan.com.cn>
2015-08-12 13:29:39 +0800

    update

--- Envfile.js
+++ Envfile.js
@@ -1,4 +1,10 @@
 module.exports = function(task){
 	task.js("compress-js", "./index.js", "./dist/minify.js");
+	task.js('demo-js-normal', './test/res/js/compent.js', './test/res/js/dist.js');
+	task.css("demo-css-normal", { "./test/res/css/dist.css": [
+		"./test/res/css/bootstrap.css",
+		"./test/res/css/font-awesome.css"
+	] });
 	task.registTask("default", ["compress-js"]);
+	task.registTask('demo', ['demo-css-normal', 'demo-js-normal']);
 };
---
+++ test/res/js/compent.js
@@ -0,0 +1,6 @@
+/**
+ * Created by evio on 15/8/12.
+ */
+var jQuery = require('./jquery-2.1.4.min');
+var bootstrap = require('./bootstrap');
+window.jQuery = window.$ = jQuery;
