7e40dd0b5becf3c2721b25d52541101e57e9cc8f
evio <shenyunjie@51zhangdan.com.cn>
2015-09-28 20:02:38 +0800

    update

--- src/directives/so-on.js
+++ src/directives/so-on.js
@@ -8,8 +8,8 @@ export default (attr, DOM, vm) => {
     object.value = {};
     object.set = (method, foo) => object.value[method] = foo;
 
-    object.render = function(scope){
-        this.scope = scope || this.scope;
+    object.render = function(scope = this.scope, options = {}){
+        this.scope = scope;
         var contexts = this.expression.split('|');
         contexts.forEach(context => {
             var gate = context.indexOf(':');
@@ -19,14 +19,22 @@ export default (attr, DOM, vm) => {
             if ( typeof this.value[method] !== 'function' ){
                 this.node.addEventListener(method, (...args) => {
                     var fn = this.value[method];
-                    var result = fn.call(this.node, this.scope);
+                    var result = fn.call(this.node, this.scope, options);
                     if ( typeof result === 'function' ){
                         result.apply(this.node, args);
                     }
                 }, false);
             }
 
-            this.set(method, new Function('scope', ';with(scope){\nreturn ' + express + '\n};'));
+            let str = [];
+            for ( var key in options ) {
+                str.push("var " + key + " = $options['" + key + "'];");
+            }
+
+            var foo = new Function('$this', '$options',
+                str.join('\n') + '\nwith($this){return ' + express + '}');
+
+            this.set(method, foo);
         });
     };
 
