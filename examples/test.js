6d4d8a3d523274dbfe9c6e41fc677fe9f3aecdbf
evio <shenyunjie@51zhangdan.com.cn>
2015-10-10 22:13:32 +0800

    update

--- examples/test.js
+++ examples/test.js
@@ -4,113 +4,13 @@
 require('./test.html');
 var soyie = require('../src/index');
 
-
-soyie.component('child', {
-    props: ['msg'],
-    events: {
-        init(){
-            console.log('+1')
-        }
-    }
-});
-
-soyie.component('task', {
-    props: {
-        tasks: {
-            type: ['Array'],
-            required: true
-        },
-        news:{}
-    },
-    events: {
-        init: function(){
-            //console.log(1)
-        }
-    },
-    handle: function(scope){
-        scope.remind = function(){
-            var i = scope.tasks.length, j = i;
-            while (i--){
-                if ( scope.tasks[i].state == '1' ){
-                    j--;
-                }
-            }
-            return j;
-        };
-        scope.newtask = '';
-        scope.addtask = function(){
-            scope.tasks.push({
-                name: scope.newtask,
-                state: 0
-            });
-            scope.newtask = '';
-        }
-    },
-    template: '<p>你还有{{remind()}}个任务没完成</p>'
-    +           '<ul>'
-    +                   '<li so-repeat="{{tasks}}">'
-    +                       '<input type="checkbox" so-binding="state" so-unchecked="0" value="1">'
-    +                       '<span class="{{state == \'1\' ? \'line\': \'\'}}">{{name}}</span>'
-    +                   '</li>'
-    +           '</ul>'
-    +           '<input type="text" so-binding="newtask" placeholder="input your task..." />'
-    +           '<button so-on="click:addtask">add</button>'
-});
-function c1(){
-    alert(1);
-}
-function c2(){
-    alert(2)
-}
 soyie.ready(() => {
     soyie.bootstrap('app', function(){
-        this.list = [
-            { a:1, b:2, c: ['a', 'b'], d:c1  },
-            { a:3, b:4, c: ['c', 'd'], d:c1 },
-            { a:5, b:6, c: ['e', 'f'], d:c1 },
-            { a:7, b:8, c: ['g', 'h'], d:c1 },
-            { a:9, b:10, c: ['i', 'j'], d:c1 }
-        ];
-        this.title = "evio";
-        this.change = function(){
-            alert(1);
+        var that = this;
+        var data = { phoneNumber: '', password: '' };
+        data.submit = function(){
+            console.log(that)
         };
-        this.name = 'molly';
-        this.tasks = [
-            { name: "今天要将soyie一期完成", state: 0 },
-            { name: "今天要将soyie二期完成", state: 0 },
-            { name: "今天要将soyie三期完成", state: 0 },
-            { name: "今天要将soyie四期完成", state: 0 }
-        ];
-        this.aaa = [1,2,3,4,5,6];
-        setTimeout(() => {
-            this.list = [
-                { a:16, b:26, c: ['ccc', 'ccdfaf'],d:c2 },
-                { a:4, b:46, c: ['dsaf', 'adfadsf'],d:c2 },
-                { a:56, b:66, c: ['esadf', 'fdas'],d:c2 },
-                { a:76, b:8, c: ['gdsaf', 'hadsf'],d:c2 },
-                { a:96, b:610, c: ['isadf', 'jdsaf'], d:c2 }
-            ];
-            setTimeout(() =>{
-                this.list[0].c = ['dafasfadfa','sadfsdfa'];
-            }, 2000);
-        }, 2000);
+        this.tree = data;
     });
-    //var vm = soyie.app('app');
-    //vm.init(scope);
-    //scope.list = [
-    //    { a:1, b:2, c: ['a', 'b'] },
-    //    { a:3, b:4, c: ['c', 'd'] },
-    //    { a:5, b:6, c: ['e', 'f'] },
-    //    { a:7, b:8, c: ['g', 'h'] },
-    //    { a:9, b:10, c: ['i', 'j'] }
-    //];
-    //setTimeout(() => {
-    //    scope.tasks = [
-    //        { name: "今天要将soyie一期完成", state: 0 }
-    //    ];
-    //    setTimeout(function(){
-    //        scope.tasks.push({ name: "今天要将soyie二期完成", state: 0 })
-    //    }, 1000)
-    //}, 1000);
 });
--- src/directives/so-on.js
+++ src/directives/so-on.js
@@ -6,36 +6,41 @@ export default (attr, DOM, vm) => {
     var object = new node(DOM, expression);
     object.parent = vm;
     object.value = {};
-    object.set = (method, foo) => object.value[method] = foo;
+    object.options = {};
+    object.set = (method, foo) => object.value[method].foo = foo;
+    object.node.events = object.node.events || [];
+
+    object.expression.split('|').forEach(context => {
+        var gate = context.indexOf(':');
+        var method = context.substring(0, gate);
+        var express = context.substring(gate + 1).trim();
+        if ( object.node.events.indexOf(method) == -1 ){
+            object.value[method] = { expression: express, foo: null };
+            object.node.addEventListener(method, (...args) => {
+                var use = object.value[method];
+                if ( !use ) return;
+                var fn = use.foo;
+                if ( typeof fn === 'function' ){
+                    var result = fn.call(object.node, object.scope, object.options);
+                    if ( typeof result === 'function' ){
+                        result.apply(object.node, args);
+                    }
+                }
+            }, false);
+            object.node.events.push(method);
+        }
+    });
 
     object.render = function(scope = this.scope, options = {}){
-        this.scope = scope;
-        var contexts = this.expression.split('|');
-        contexts.forEach(context => {
-            var gate = context.indexOf(':');
-            var method = context.substring(0, gate);
-            var express = context.substring(gate + 1).trim();
-
-            if ( typeof this.value[method] !== 'function' ){
-                this.node.addEventListener(method, (...args) => {
-                    var fn = this.value[method];
-                    var result = fn.call(this.node, this.scope, options);
-                    if ( typeof result === 'function' ){
-                        result.apply(this.node, args);
-                    }
-                }, false);
-            }
-
-            let str = [];
-            for ( var key in options ) {
-                str.push("var " + key + " = $options['" + key + "'];");
-            }
-
-            var foo = new Function('$this', '$options',
-                str.join('\n') + '\nwith($this){return ' + express + '}');
-
-            this.set(method, foo);
-        });
+        object.scope = scope;
+        object.options = options;
+        let str = [];
+        for ( var key in object.options ) { str.push("var " + key + " = $options['" + key + "'];"); }
+        for ( let method in object.value ){
+            this.set(method,
+                new Function('$this', '$options', str.join('\n') + '\nwith($this){return ' + object.value[method].expression + '}')
+            );
+        }
     };
 
     DOM.removeAttribute('so-on');
